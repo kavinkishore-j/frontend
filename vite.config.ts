478473255b2240af0c49ad50e940a44f6dee1d97
kavinkishorej-ui <kavinkishore.j@gmail.com>
2025-11-12 22:44:30 +0530

    update: latest project fixes and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/restaurent/',   // <-- important for GitHub Pages
-  plugins: [react()]
-})
+  base: '/',
+  plugins: [react()],
+});
